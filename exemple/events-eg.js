3c320f2b8e8bab1b66502e5286167806ac266fc1
Cyriaque-TONNERRE <cyriaque.tonnerre@student.junia.com>
2022-07-31 12:46:28 +0200

    Création tuto

---
+++ exemple/events-eg.js
@@ -0,0 +1,22 @@
+/*---------------------------Outils-alternatif-----------------------------*/
+
+const {QuickDB} = require("quick.db");  //Permet d'importer la base de données
+const db = new QuickDB(); //Permet d'importer la base de données
+const user_db = db.table("user"); //Permet d'importer la base de données
+
+const {Nom_de_variable} = require("../config.json"); //Import d'une variable du fichier config.json
+
+const {EmbedBuilder} = require("discord.js"); //Permet de créer des embeds
+
+const {ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js'); //Permet de créer des boutons
+
+const {verificationpermission} = require("../fonctions/verificationpermission"); //Import de la fonction de verification de permission
+
+/*-------------------------------------------------------------------------*/
+
+module.exports = {
+    name: "nom_de_l'event",
+     execute(resultat_de_levent(member/channel/role/etc)) {
+        // Ce que l'event va exécuté si il est détecter sur discord
+    }
+}
---
+++ exemple/fonctions-eg.js
@@ -0,0 +1,7 @@
+function nom_de_la_fonction(argument_requis, second_argument, etc) {
+    //ce que la fonction va faire
+}
+
+module.exports = {nom_de_la_fonction}; //Export de la fonction
+
+const {nom_de_la_fonction} = require("../exemple/fonctions-eg.js"); //exemple d'import de notre fonction
