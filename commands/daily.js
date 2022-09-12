69fc8a743e791c18f5b953cb32829186d7045999
Cyriaque-TONNERRE <cyriaque.tonnerre@student.junia.com>
2022-09-12 23:05:20 +0200

    Daily tous les jours et plus toutes les 24h

--- commands/daily.js
+++ commands/daily.js
@@ -11,14 +11,13 @@ module.exports = {
 
     async execute(interaction) {
         if (await user_db.get(interaction.user.id + `.lastDaily`) === null) {
-            await user_db.set(interaction.user.id + `.lastDaily`, new Date().getTime());
+            await user_db.set(interaction.user.id + `.lastDaily`, new Date().getDate());
             await user_db.add(interaction.user.id + `.money`, 125);
             interaction.reply({content:`Vous avez reçu 125 <a:octet:1010177758250405888> !`, ephemeral: true});
         } else {
             const lastDaily = await user_db.get(interaction.user.id + `.lastDaily`);
-            const diff = new Date().getTime() - lastDaily;
-            if (diff >= 86400000) {
-                await user_db.set(interaction.user.id + `.lastDaily`, new Date().getTime());
+            if (new Date().getDate() !== lastDaily) {
+                await user_db.set(interaction.user.id + `.lastDaily`, new Date().getDate());
                 await user_db.add(interaction.user.id + `.money`, 125);
                 interaction.reply({content:`Vous avez reçu 125 <a:octet:1010177758250405888> !`, ephemeral: true});
             } else {
