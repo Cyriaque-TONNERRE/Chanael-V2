36b2d40f51336f5061ced459b194fa133ca61671
Cyriaque-TONNERRE <cyriaque.tonnerre@student.junia.com>
2022-09-12 14:30:30 +0200

    Pas d'xp dans certain channel

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,7 +3,7 @@ const {register_user} = require("../fonctions/register_user");
 const {randomInt} = require("mathjs");
 const {lvl_up} = require("../fonctions/lvl_up");
 const {EmbedBuilder} = require("discord.js");
-const {channelBotId, clientId} = require("../config.json");
+const {channelBotId, clientId, categoryNoXpNoMoney} = require("../config.json");
 const db = new QuickDB();
 
 const user_db = db.table("user");
@@ -12,31 +12,33 @@ module.exports = {
     name: 'messageCreate',
     async execute(message) {
         if (message.author.bot) return;
-        if (!await user_db.has(message.author.id)) {
-            register_user(message.author.id).then(async () => {
-                await user_db.set(message.author.id + ".money", randomInt(5, 16));
-                await user_db.set(message.author.id + ".xp", randomInt(3, 12));
-                await user_db.set(message.author.id + ".lastMessage", new Date().getTime());
-            });
-        } else {
-            if (await user_db.get(message.author.id + ".lastMessage") + 1000 < new Date().getTime()) {
-                const lvl = await user_db.get(message.author.id + ".level");
-                await user_db.add(message.author.id + ".money", randomInt(5,16));
-                await user_db.set(message.author.id + ".lastMessage", new Date().getTime());
-                user_db.add(message.author.id + ".xp", randomInt(3,12)).then(() => {
-                    lvl_up(message.guild, message.author.id).then((isUP) => {
-                        if (isUP) {
-                            let lvlupEmbed = new EmbedBuilder()
-                                .setColor('#0162b0')
-                                .setTitle(`Bravo ${message.guild.members.cache.get(message.author.id).displayName} !`)
-                                .setThumbnail(message.author.displayAvatarURL())
-                                .setDescription(`Vous avez atteint le niveau ${lvl + 1} !`)
-                                .setTimestamp()
-                                .setFooter({ text: 'Chanael', iconURL: message.guild.members.cache.get(clientId).displayAvatarURL()});
-                            message.guild.channels.cache.get(channelBotId).send({embeds: [lvlupEmbed]});
-                        }
-                    });
+        if (!categoryNoXpNoMoney.hasAny(...message.channel.parentId)){
+            if (!await user_db.has(message.author.id)) {
+                register_user(message.author.id).then(async () => {
+                    await user_db.set(message.author.id + ".money", randomInt(5, 16));
+                    await user_db.set(message.author.id + ".xp", randomInt(3, 12));
+                    await user_db.set(message.author.id + ".lastMessage", new Date().getTime());
                 });
+            } else {
+                if (await user_db.get(message.author.id + ".lastMessage") + 1000 < new Date().getTime()) {
+                    const lvl = await user_db.get(message.author.id + ".level");
+                    await user_db.add(message.author.id + ".money", randomInt(5,16));
+                    await user_db.set(message.author.id + ".lastMessage", new Date().getTime());
+                    user_db.add(message.author.id + ".xp", randomInt(3,12)).then(() => {
+                        lvl_up(message.guild, message.author.id).then((isUP) => {
+                            if (isUP) {
+                                let lvlupEmbed = new EmbedBuilder()
+                                    .setColor('#0162b0')
+                                    .setTitle(`Bravo ${message.guild.members.cache.get(message.author.id).displayName} !`)
+                                    .setThumbnail(message.author.displayAvatarURL())
+                                    .setDescription(`Vous avez atteint le niveau ${lvl + 1} !`)
+                                    .setTimestamp()
+                                    .setFooter({ text: 'Chanael', iconURL: message.guild.members.cache.get(clientId).displayAvatarURL()});
+                                message.guild.channels.cache.get(channelBotId).send({embeds: [lvlupEmbed]});
+                            }
+                        });
+                    });
+                }
             }
         }
     }
