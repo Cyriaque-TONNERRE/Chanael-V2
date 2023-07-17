3d274a412b5b3bd51530181f778d748323e99190
Cyriaque-TONNERRE <cyriaue.tonnerre@student.junia.com>
2023-07-17 15:33:38 +0200

    Petite Modif

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports = {
         .setName('help')
         .setDescription('Affiche la liste des commandes.'),
     async execute(interaction) {
-        const commands = interaction.client.commands.filter(command => command.data.description !== undefined);
+        const commands = interaction.client.commands.filter(command => command.data.description !== undefined).filter(command => command.data.description !== "undefined");
         await interaction.reply({
             content: `📚 Liste des commandes :\n\n${commands.map(command => `**${command.data.name}** : ${command.data.description}`).join('\n')}`,
             ephemeral: true
